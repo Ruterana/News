367f17379f217e0bbd6939ed5fd33e52949354dd
Ruterana <ncharlene38@gmail.com>
2019-09-13 16:28:46 +0200

    i details of articleson config.py

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,6 @@
 import urllib.request,json
 from .models import Sources
+from .models import Articles
 # Getting api key
 api_key = None
 #Getting the news base url
@@ -9,6 +10,7 @@ def configure_request(app):
     api_key = app.config['SOURCE_API_KEY']
     print(api_key)
     base_url = app.config['SOURCES_API_BASE_URL']
+    bases_url = app.config['ARTICLE_API_BASE_URL'] 
 def get_sources(category):
     '''
     Function that gets the json response to our url request
@@ -55,4 +57,48 @@ def process_results(sources_list):
             sources_results.append(sources_object)
 
     return sources_results
+def get_articles(id):
+    '''
+    Function that gets the json response to our url request
+    '''
+   get_articles_url = base_url.format(id,api_key)
 
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = Process_results(articles_results_list)
+
+
+    return articles_results
+def Proces_results(articles_list):
+    '''
+    Function  that processes the articles result and transform them to a list of Objects
+
+    Args:
+    articles_list: A list of dictionaries that contain articles details
+
+    Returns :
+    sources_results: A list ofarticles objects
+    '''
+    articles_results = []
+    for articles_item in articles_list:
+        
+        id = articles_item.get('id')
+        name = articles_item.get('name')
+        author = articles_item.get('author')
+        title = articles_item.get('title')
+        description = articles_item.get('description')
+        url = articles_item.get('url')
+        urlToImage = articles_item.get('urlToImage')
+        publicedAt = articles_item.get('publicedAt')
+        content = articles_item.get('content')
+        if name:
+            articles_object = Sources(id,name,author,title,description,url,urlToImage,publicedAt,content)
+            articles_results.append(articles_object)
+
+    return articles_results
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     SOURCE_API_KEY = 'ad6606b55c7d4d83bf3cd45680c71637'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     print(SOURCE_API_KEY)
+    ARTICLE_API_BASE_URL='https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey={}'
 
 class ProdConfig(Config):
     pass
@@ -18,8 +19,5 @@ config_options = {
 'development':DevConfig,
 'production':ProdConfig
 }
-###### for article#########
-ARTICLE_API_BASE_URL='https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey={}'
-ARTICLE_API_KEY = 'ad6606b55c7d4d83bf3cd45680c71637'
-SECRET_KEY = os.environ . get ('SECRET_KEY')
-print (ARTICLE_API_KEY )
+
+
